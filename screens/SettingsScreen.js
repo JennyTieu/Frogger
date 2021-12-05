d4c2e2c39fe65f5b33850ae5a3e40c12b4e2112e
Jasmin <jasmin.seeliger@googlemail.com>
2021-12-05 18:59:21 +0100

    update Settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,24 +1,26 @@
 import React, {useContext, useState} from 'react';
-import {View, Text, FlatList, StyleSheet, Button} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import {Context} from '../data/Context';
 import {AuthContext} from '../data/AuthContext';
+import {Button} from "react-native-elements";
+import {Ionicons} from "@expo/vector-icons";
 
 export default SettingsScreen = () => {
   const { signOut } = useContext(AuthContext);
   const [profileData] = useContext(Context);
-  const ids =profileData.profiles.filter(item => item.id);
+  const ids = profileData.profiles.filter(item => item.id);
 
   return(
-    <View>
-      <View>
+    <View style={styles.screenContainer}>
+      <View style={styles.topContainer}>
         <Text>SettingsScreen</Text>
         <Text>Signed in!</Text>
-        <Button title="Sign out" onPress={signOut} />
       </View>
-      <View>
-        
+      <View style={styles.middleContainer}>
+        <Button title="Design "type="clear" icon={<Ionicons name="md-color-palette" size={28}/>} onPress={signOut}/>
+        <Button title="Sign out" type="clear" icon={<Ionicons name="md-exit-outline" size={28}/>} onPress={signOut}/>
       </View>
-      <View>
+      <View style={styles.bottomContainer}>
         
       </View>
     </View>
@@ -26,7 +28,17 @@ export default SettingsScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screenContainer: {
+    height: "100%",
+    justifyContent:"space-evenly"
+  },
+  topContainer: {
+    flex: 1
+  },
+  middleContainer: {
+    flex: 3
+  },
+  bottomContainer: {
+    flex: 1
   },
 });
