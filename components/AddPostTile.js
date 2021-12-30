f45a634d1ee4de831afbae1544806ecbfeaa2864
JennyTieu <57066145+JennyTieu@users.noreply.github.com>
2021-12-30 15:41:48 +0100

    addPost

--- components/AddPostTile.js
+++ components/AddPostTile.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from "react";
+import React, { useState,useContext , useEffect} from "react";
 import { StyleSheet, View, TextInput ,Text, TouchableOpacity, Image, ScrollView} from "react-native";
 import { Button } from "react-native-elements";
 import { Ionicons } from "@expo/vector-icons";
@@ -7,13 +7,237 @@ import { Context } from "../data/Context";
 import moment from "moment";
 import { useTheme } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView } from "react-native-web";
 
 export default AddPostTile =(props) =>{
+    const [id, setId] = useState("")
+    useEffect(async () => {
+        try {
+          const value = await AsyncStorage.getItem('storedId')
+          if(value !== null) {
+            // value previously stored
+          }
+          setId(value);
+          setId(value);
+        } catch(e) {
+          // error reading value
+        }
+      }, [])
+
+    const { colors } = useTheme();
+    const [profileData,setProfileData] = useContext(Context);
+    const [loggedUser] = profileData.profiles.filter(item => item.id.includes(id));
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [valueMS, setValueMS] = useState([]);
+    const [textInput, setTextInput] = useState("");
+    let numOfLines =0;
+
+    const onChangeMS = (value) => {
+        setValueMS(value);
+    };
+
+    const changeTextHandler=(enteredText) =>{
+        setTextInput(enteredText);
+    };
+
+    const showImagePicker = async () => {
+        // Ask the user for the permission to access the media library 
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("You've refused to allow this appp to access your photos!");
+          return;
+        }
+    
+        const result = await ImagePicker.launchImageLibraryAsync();
+    
+        // Explore the result
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setSelectedImage({ localUri: result.uri });
+        }
+      }
+    
+      const openCamera = async () => {
+        // Ask the user for the permission to access the camera
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("You've refused to allow this appp to access your camera!");
+          return;
+        }
+    
+        const result = await ImagePicker.launchCameraAsync();
+    
+        // Explore the result
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setSelectedImage({ localUri: result.uri });
+        }
+      }
+
     return(
-        <ScrollView>
-            <View>
-                <Text>new Post</Text>
+        <View style={styles.full}>
+            <ScrollView style={{width:'100%', backgroundColor:"#f1f2f6" ,  flexWrap:'wrap'}}>
+                <View style={{width:'100%',flexDirection:'row'}}>
+                    <View style={styles.ppView}>
+                        <Image source={loggedUser.image} style={styles.profileImage}/>
+                    </View>
+                    <View style={styles.screen}>
+                        <View style={styles.topContainer}>
+                            <TextInput
+                                multiline={true}
+                                numberOfLines={numOfLines}
+                                placeholder="What's happening?"
+                                style={styles.inputBox}
+                                underlineColorAndroid = "transparent"
+                                autoCapitalize = "none"
+                                onChangeText={changeTextHandler}
+                                value={textInput}
+                                onContentSizeChange={(e) => {
+                                    numOfLines = e.nativeEvent.contentSize.height/18;
+                                }}
+                                />
+                        </View>
+                        <View style={styles.bottomContainer}>
+                            <View style={[styles.cameraPreviewBlank]}>
+                                {selectedImage === null?(
+                                    <View/>
+                                ) : (
+                                    <TouchableOpacity onPress={() => setSelectedImage(null)}>
+                                    <Image
+                                        source={{ uri: selectedImage.localUri }}
+                                        style={styles.cameraPreview}
+                                    />
+                                    </TouchableOpacity>
+                                )}
+                            </View>
+                            
+                        </View>
+                    </View>
+                </View>
+                
+            </ScrollView>
+            <View style={styles.lowerBox}>
+                <View style={styles.buttonContainer}>
+                    <Button type= "clear"
+                        icon={
+                            <Ionicons
+                                name="md-images"
+                                size={40}
+                                color="gray"
+                            />}
+                        onPress={showImagePicker} />
+                    <Button type= "clear" 
+                        icon={
+                            <Ionicons
+                                name="md-camera"
+                                size={40}
+                                color="gray"
+                            />} 
+                        onPress={openCamera} />
+                </View>
             </View>
-        </ScrollView>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    ppView:{
+      alignSelf:'flex-start',
+      width:'13%',
+      alignItems:'center',
+      padding:10
+    },
+    screen: {
+        alignSelf:'flex-start',
+      flexWrap:'wrap',
+      flexDirection:'row',
+      padding:10,
+      width:'87%',
+      backgroundColor: "#f1f2f6",
+      alignItems: "center",
+    },
+    topContainer: {
+      width:'100%',
+      alignItems:'center'
+
+    },
+    bottomContainer: {
+      justifyContent:"center",
+      alignItems: "center",
+    },
+    cameraPreview: {
+      width: 200,
+      height: 200,
+      alignItems:'center',
+      justifyContent: 'center',
+      resizeMode: "contain",
+      backgroundColor:"white"
+    },
+    cameraPreviewBlank: {
+      width: 300,
+      height: 300,
+      alignItems:'center',
+      justifyContent: 'center',
+      resizeMode: "contain",
+    },
+    full: {
+      width:'100%',
+      height:'100%'
+      
+    },
+    inputBox:{
+        fontWeight: "bold",
+        width: "100%",
+        fontSize:15,
+        padding:10,
+        alignItems: "center",
+      },
+    lowerBox:{
+      width:'100%',
+      height:'10%',
+      position:'absolute',
+      justifyContent:'flex-start',
+      bottom:0,
+      borderTopWidth:0.5,
+      borderTopColor:'gray'
+    },
+    verticalContainer: {
+      alignItems: "center",
+    },
+    buttonContainer:{
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+    },
+    container: {
+      paddingTop: 20,
+      paddingBottom:30,
+      marginLeft: 20,
+      marginRight: 20,
+      flex: 1,
+      width: "100%"
+    },
+    button: {
+      backgroundColor: "white",
+    },
+    buttonText: {
+      fontSize: 20,
+    },
+    buttonView: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'row',
+      marginTop: 10,
+    },
+    profileImage:{
+        height: 40,
+        width:40,
+        backgroundColor: "black",
+        borderRadius: 100,
+    },
+  });
+  
